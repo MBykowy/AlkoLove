290e652dcc59f453251865e731675c7af314a6f3
MBykowy <109218@g.elearn.uz.zgora.pl>
2024-05-22 16:08:27 +0200

    filtrowanie gotowe w 75%

--- src/index.ts
+++ src/index.ts
@@ -4,33 +4,7 @@ import { Client } from '@neondatabase/serverless';
 addEventListener("fetch", (event) => {
   event.respondWith(handleEvent(event));
 });
-//Wyszukiwarka
-async function fetchProductData(client: Client, nazwa: string) {
-  await client.connect();
 
-  const { rows: productRows } = await client.query('SELECT * FROM produkty WHERE nazwa = $1 ORDER BY cena ASC', [nazwa]);
-
-  for (let product of productRows) {
-    const { rows: shopRows } = await client.query('SELECT * FROM sklepy WHERE id_sklep = $1', [product.id_sklep]);
-    const { rows: shopAddressRows } = await client.query('SELECT * FROM sklepy_adres WHERE id_sklep = $1', [product.id_sklep]);
-    product.shop = shopRows[0];
-    if (shopAddressRows[0]) {
-      product.shop.adres = shopAddressRows[0].adres;
-    }
-  }
-
-  await client.end();
-  return productRows[0]; // return the cheapest product
-}
-//porównywarka
-async function fetchAllProducts(client: Client) {
-  await client.connect();
-
-  const { rows: productRows } = await client.query('SELECT * FROM produkty ORDER BY nazwa ASC');
-
-  await client.end();
-  return productRows;
-}
 async function handleEvent(event: FetchEvent) {
   const url = new URL(event.request.url);
   const pathname = url.pathname;
@@ -42,17 +16,61 @@ async function handleEvent(event: FetchEvent) {
       const rows = await fetchProductData(client, nazwa);
       return new Response(JSON.stringify(rows), { status: 200 });
     }
-  } else if (pathname.startsWith('/api/products')) {
+
+
+
+
+  }else if (pathname.startsWith('/porownaj/produkty')) {
     const client = new Client(DATABASE_URL);
-    const products = await fetchAllProducts(client);
-    return new Response(JSON.stringify(products), {
-      headers: { 'Content-Type': 'application/json' },
-    });
+    const rows = await fetchAllProducts(client);
+    return new Response(JSON.stringify(rows), { status: 200 });
   }
+
+
+
+
+
 
   try {
     return await getAssetFromKV(event);
   } catch (e) {
     return new Response(`"${pathname}" not found`, { status: 404, statusText: "not found" });
   }
+}
+
+
+//Wyszukiwarka
+async function fetchProductData(client: Client, nazwa: string) {
+  await client.connect();
+  const { rows: productRows } = await client.query('SELECT * FROM produkty WHERE nazwa = $1 ORDER BY cena ASC', [nazwa]);
+  for (let product of productRows) {
+    const { rows: shopRows } = await client.query('SELECT * FROM sklepy WHERE id_sklep = $1', [product.id_sklep]);
+    const { rows: shopAddressRows } = await client.query('SELECT * FROM sklepy_adres WHERE id_sklep = $1', [product.id_sklep]);
+    product.shop = shopRows[0];
+    if (shopAddressRows[0]) {
+      product.shop.adres = shopAddressRows[0].adres;
+    }
+  }
+  await client.end();
+  return productRows[0]; // return the cheapest product
+}
+
+
+
+
+
+//porównywarka
+async function fetchAllProducts(client: Client) {
+  await client.connect();
+  const { rows: productRows } = await client.query('SELECT * FROM produkty ORDER BY cena ASC');
+  for (let product of productRows) {
+    const { rows: shopRows } = await client.query('SELECT * FROM sklepy WHERE id_sklep = $1', [product.id_sklep]);
+    const { rows: shopAddressRows } = await client.query('SELECT * FROM sklepy_adres WHERE id_sklep = $1', [product.id_sklep]);
+    product.shop = shopRows[0];
+    if (shopAddressRows[0]) {
+      product.shop.adres = shopAddressRows[0].adres;
+    }
+  }
+  await client.end();
+  return productRows;
 }
