1e33e0d02c65534c0116b6d70f8e890a4c9abca7
MBykowy <109218@g.elearn.uz.zgora.pl>
2024-06-15 02:14:16 +0200

    profil w 80%

--- src/index.ts
+++ src/index.ts
@@ -1,222 +1,247 @@
   import {
-  getAssetFromKV
-} from "@cloudflare/kv-asset-handler";
-import {
-  Client
-} from '@neondatabase/serverless';
+    getAssetFromKV
+  } from "@cloudflare/kv-asset-handler";
+  import {
+    Client
+  } from '@neondatabase/serverless';
 
-addEventListener("fetch", (event) => {
-  event.respondWith(handleEvent(event));
-});
+  addEventListener("fetch", (event) => {
+    event.respondWith(handleEvent(event));
+  });
 
-async function handleEvent(event: FetchEvent) {
-  const url = new URL(event.request.url);
-  const pathname = url.pathname;
+  async function handleEvent(event: FetchEvent) {
+    const url = new URL(event.request.url);
+    const pathname = url.pathname;
 
-  if (pathname.startsWith('/wyszukiwarka')) {
-    const nazwa = url.searchParams.get('nazwa');
-    if (nazwa) {
+    if (pathname.startsWith('/wyszukiwarka')) {
+      const nazwa = url.searchParams.get('nazwa');
+      if (nazwa) {
+        const client = new Client(DATABASE_URL);
+        const rows = await fetchProductData(client, nazwa);
+        return new Response(JSON.stringify(rows), {
+          status: 200
+        });
+      }
+
+
+
+
+    } else if (pathname.startsWith('/porownaj/produkty')) {
       const client = new Client(DATABASE_URL);
-      const rows = await fetchProductData(client, nazwa);
+      const rows = await fetchAllProducts(client);
       return new Response(JSON.stringify(rows), {
         status: 200
       });
+
+
+
+
+    } else if (event.request.method === 'POST' && pathname === '/register') {
+      const data = await event.request.json(); // parse the incoming JSON
+      const client = new Client(DATABASE_URL);
+      const result = await registerUser(client, data);
+      console.log(result); // log the result before returning it
+      return new Response(JSON.stringify(result), {
+        status: 200
+      });
+
+    } else if (event.request.method === 'POST' && pathname === '/login') {
+      const data = await event.request.json(); // parse the incoming JSON
+      const client = new Client(DATABASE_URL);
+      const result = await loginUser(client, data);
+      console.log(result); // log the result before returning it
+      return new Response(JSON.stringify(result), {
+        status: 200
+      });
+    } else if (pathname.startsWith('/filtrowanie/produkty')) {
+      const client = new Client(DATABASE_URL);
+      const rows = await fetchAllProducts(client);
+      return new Response(JSON.stringify(rows), {
+        status: 200
+      });
+    } else if (pathname.startsWith('/sklepy/lokalizacje')) {
+      const client = new Client(DATABASE_URL);
+      const rows = await fetchAllShopLocalizations(client);
+      return new Response(JSON.stringify(rows), {
+        status: 200
+      });
+    } else if (event.request.method === 'POST' && pathname === '/profil/dane') {
+      const client = new Client(DATABASE_URL);
+      const userId = await event.request.json(); // parse the incoming JSON
+      const userData = await fetchUserProfile(client, userId);
+      return new Response(JSON.stringify({
+        data: userData
+      }), {
+        status: 200
+      });
+    } else if (event.request.method === 'POST' && pathname === '/profil/zapisz') {
+      const client = new Client(DATABASE_URL);
+      await ZmienDane(client, event.request);
+      
+      return new Response('success', { status: 200 });
     }
 
 
 
 
-  } else if (pathname.startsWith('/porownaj/produkty')) {
-    const client = new Client(DATABASE_URL);
-    const rows = await fetchAllProducts(client);
-    return new Response(JSON.stringify(rows), {
-      status: 200
-    });
 
 
-
-
-  } else if (event.request.method === 'POST' && pathname === '/register') {
-    const data = await event.request.json(); // parse the incoming JSON
-    const client = new Client(DATABASE_URL);
-    const result = await registerUser(client, data);
-    console.log(result); // log the result before returning it
-    return new Response(JSON.stringify(result), {
-      status: 200
-    });
-
-  } else if (event.request.method === 'POST' && pathname === '/login') {
-    const data = await event.request.json(); // parse the incoming JSON
-    const client = new Client(DATABASE_URL);
-    const result = await loginUser(client, data);
-    console.log(result); // log the result before returning it
-    return new Response(JSON.stringify(result), {
-      status: 200
-    });
-  } else if (pathname.startsWith('/filtrowanie/produkty')) {
-    const client = new Client(DATABASE_URL);
-    const rows = await fetchAllProducts(client);
-    return new Response(JSON.stringify(rows), {
-      status: 200
-    });
-  } else if (pathname.startsWith('/sklepy/lokalizacje')) {
-    const client = new Client(DATABASE_URL);
-    const rows = await fetchAllShopLocalizations(client);
-    return new Response(JSON.stringify(rows), {
-      status: 200
-    });
-  }else if (event.request.method === 'POST' && pathname === '/profil') {
-    const client = new Client(DATABASE_URL);
-    const userId = await event.request.json(); // parse the incoming JSON
-    const userData = await fetchUserData(client, userId);
-    const userPhoto = await fetchUserPhoto(client, userId);
-    return new Response(JSON.stringify({data: userData, photo: userPhoto}), {
-      status: 200
-    });
-}
-
-
-
-
-
-
-  try {
-    return await getAssetFromKV(event);
-  } catch (e) {
-    return new Response(`"${pathname}" not found`, {
-      status: 404,
-      statusText: "not found"
-    });
+    try {
+      return await getAssetFromKV(event);
+    } catch (e) {
+      return new Response(`"${pathname}" not found`, {
+        status: 404,
+        statusText: "not found"
+      });
+    }
   }
-}
 
 
-//Wyszukiwarka
-async function fetchProductData(client: Client, nazwa: string) {
-  await client.connect();
-  const {
-    rows: productRows
-  } = await client.query(`
+  //Wyszukiwarka
+  async function fetchProductData(client: Client, nazwa: string) {
+    await client.connect();
+    const {
+      rows: productRows
+    } = await client.query(`
     SELECT * 
     FROM produkty 
     WHERE nazwa % $1 
     ORDER BY cena ASC, nazwa <-> $1 ASC
   `, [nazwa]);
-  for (let product of productRows) {
-    const {
-      rows: shopRows
-    } = await client.query('SELECT * FROM sklepy WHERE id_sklep = $1', [product.id_sklep]);
-    const {
-      rows: shopAddressRows
-    } = await client.query('SELECT * FROM sklepy_adres WHERE id_sklep = $1', [product.id_sklep]);
-    product.shop = shopRows[0];
-    if (shopAddressRows[0]) {
-      product.shop.adres = shopAddressRows[0].adres;
+    for (let product of productRows) {
+      const {
+        rows: shopRows
+      } = await client.query('SELECT * FROM sklepy WHERE id_sklep = $1', [product.id_sklep]);
+      const {
+        rows: shopAddressRows
+      } = await client.query('SELECT * FROM sklepy_adres WHERE id_sklep = $1', [product.id_sklep]);
+      product.shop = shopRows[0];
+      if (shopAddressRows[0]) {
+        product.shop.adres = shopAddressRows[0].adres;
+      }
     }
+    await client.end();
+    return productRows[0];
   }
-  await client.end();
-  return productRows[0]; 
-}
 
 
 
 
-//porównywarka
-async function fetchAllProducts(client: Client) {
-  await client.connect();
-  const {
-    rows: productRows
-  } = await client.query('SELECT * FROM produkty ORDER BY nazwa ASC');
-  for (let product of productRows) {
+  //porównywarka
+  async function fetchAllProducts(client: Client) {
+    await client.connect();
     const {
-      rows: shopRows
-    } = await client.query('SELECT * FROM sklepy WHERE id_sklep = $1', [product.id_sklep]);
-    const {
-      rows: shopAddressRows
-    } = await client.query('SELECT * FROM sklepy_adres WHERE id_sklep = $1', [product.id_sklep]);
-    product.shop = shopRows[0];
-    if (shopAddressRows[0]) {
-      product.shop.adres = shopAddressRows[0].adres;
+      rows: productRows
+    } = await client.query('SELECT * FROM produkty ORDER BY nazwa ASC');
+    for (let product of productRows) {
+      const {
+        rows: shopRows
+      } = await client.query('SELECT * FROM sklepy WHERE id_sklep = $1', [product.id_sklep]);
+      const {
+        rows: shopAddressRows
+      } = await client.query('SELECT * FROM sklepy_adres WHERE id_sklep = $1', [product.id_sklep]);
+      product.shop = shopRows[0];
+      if (shopAddressRows[0]) {
+        product.shop.adres = shopAddressRows[0].adres;
+      }
     }
+    await client.end();
+    return productRows;
   }
-  await client.end();
-  return productRows;
-}
 
 
-//Rejestracja
-async function registerUser(client: Client, data: any) {
-  await client.connect();
-  const query = 'INSERT INTO uzytkownicy (admin, email, haslo, nazwa) VALUES ($1, $2, $3, $4)';
-  const values = [false, data.email, data.haslo, data.nazwa]; // assuming admin is always false for new users
-  console.log
-  await client.query(query, values);
-  await client.end();
-  return {
-    status: 'success'
-  };
-}
-
-
-//Znalezienie wszystkich lokalizacji sklepów
-async function fetchAllShopLocalizations(client: Client) {
-  await client.connect();
-  const { rows: shopLocalizationRows } = await client.query('SELECT adres FROM sklepy_adres');
-  console.log('Shop Localization Rows:', shopLocalizationRows); // Logging stringified rows
-  await client.end();
-  return shopLocalizationRows;
-}
-
-//Logowanie
-async function loginUser(client: Client, data: any) {
-  await client.connect();
-  const query = 'SELECT * FROM uzytkownicy WHERE email = $1 AND haslo = $2';
-  const values = [data.email, data.password];
-  const {
-    rows
-  } = await client.query(query, values);
-  await client.end();
-
-  if (rows.length > 0) {
-    // Login was successful
+  //Rejestracja
+  async function registerUser(client: Client, data: any) {
+    await client.connect();
+    const query = 'INSERT INTO uzytkownicy (admin, email, haslo, nazwa) VALUES ($1, $2, $3, $4)';
+    const values = [false, data.email, data.haslo, data.nazwa]; // assuming admin is always false for new users
+    console.log
+    await client.query(query, values);
+    await client.end();
     return {
-      status: 'success',
-      user: rows[0].nazwa
-    };
-  } else {
-    // Invalid email or password
-    return {
-      status: 'error',
-      message: 'Invalid email or password'
+      status: 'success'
     };
   }
-}
 
-//profil
-async function fetchUserData(client: Client, userId: string) {
-  await client.connect();
 
-  const res = await client.query('SELECT * FROM users WHERE id = $1', [userId]);
-
-  await client.end();
-
-  if (res.rows.length > 0) {
-      return res.rows[0];
-  } else {
-      throw new Error('User not found');
+  //Znalezienie wszystkich lokalizacji sklepów
+  async function fetchAllShopLocalizations(client: Client) {
+    await client.connect();
+    const {
+      rows: shopLocalizationRows
+    } = await client.query('SELECT adres FROM sklepy_adres');
+    console.log('Shop Localization Rows:', shopLocalizationRows); // Logging stringified rows
+    await client.end();
+    return shopLocalizationRows;
   }
-}
 
-async function fetchUserPhoto(client: Client, userId: string) {
-  await client.connect();
+  //Logowanie
+  async function loginUser(client: Client, data: any) {
+    await client.connect();
+    const query = 'SELECT * FROM uzytkownicy WHERE email = $1 AND haslo = $2';
+    const values = [data.email, data.password];
+    const {
+      rows
+    } = await client.query(query, values);
+    await client.end();
 
-  const res = await client.query('SELECT photo FROM user_photos WHERE user_id = $1', [userId]);
-
-  await client.end();
-
-  if (res.rows.length > 0) {
-      return res.rows[0].photo;
-  } else {
-      throw new Error('User photo not found');
+    if (rows.length > 0) {
+      // Login was successful
+      return {
+        status: 'success',
+        user: rows[0].nazwa
+      };
+    } else {
+      // Invalid email or password
+      return {
+        status: 'error',
+        message: 'Invalid email or password'
+      };
+    }
   }
-}
+
+  //profil
+  async function fetchUserProfile(client: Client, userIdObj: {
+    userId: string
+  }) {
+    const userId = userIdObj.userId;
+    console.log('fetchUserProfile userId:', userId); // Log userId
+
+    await client.connect();
+
+    const userDataRes = await client.query('SELECT * FROM uzytkownicy WHERE nazwa = $1', [userId]);
+    console.log('User data response:', userDataRes.rows); // Log returned data
+    /*
+      // Log the contents of the returned data
+      if (userDataRes.rows.length > 0) {
+        console.log('Returned data contents:', userDataRes.rows[0]);
+      } else {
+        console.log('No data returned');
+      }
+      */
+
+    await client.end();
+
+    if (userDataRes.rows.length > 0) {
+      return {
+        data: userDataRes.rows[0],
+      };
+    } else {
+      throw new Error('User data not found');
+    }
+  }
+
+  async function ZmienDane(client: Client, data1: any) {
+    await client.connect();
+    const query = 'UPDATE uzytkownicy SET admin = $1, email = $2, haslo = $3, nazwa = $4, zdjecie = $5 WHERE nazwa = $4';
+    console.log(data1.email);
+    const values = [false, data1.email, data1.haslo, data1.nazwa, data1.zdjecie]; // assuming admin is always false for new users
+    await client.query(query, values);
+    await client.end();
+    return {
+      status: 200,
+      statusText: 'OK',
+      headers: {},
+      body: JSON.stringify({ status: 'success' })
+    };
+  }
